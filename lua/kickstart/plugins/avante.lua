ba186378fd6ec7fb2c3407a5c1bf5ef8edd8e107
Pol Milian <pol.milian.dev@gmail.com>
2025-02-11 01:45:42 +0800

    fix

--- lua/kickstart/plugins/avante.lua
+++ lua/kickstart/plugins/avante.lua
@@ -4,6 +4,7 @@ return {
   lazy = false,
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  opts = {},
   build = 'make',
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
