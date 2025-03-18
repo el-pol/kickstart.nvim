2c6811002bbbdc8e9fc58965c2821fc8fab99885
Pol Milian <pol.milian.dev@gmail.com>
2025-03-18 16:46:49 +0700

    default config

--- lua/kickstart/plugins/avante.lua
+++ lua/kickstart/plugins/avante.lua
@@ -1,13 +1,13 @@
 return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
-  lazy = false,
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   opts = {},
   build = 'make',
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
+    'nvim-treesitter/nvim-treesitter',
     'stevearc/dressing.nvim',
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
