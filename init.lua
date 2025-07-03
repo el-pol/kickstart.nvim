05dff1e22f86e0568d947487e98a6329267bee79
Pol Milian <pol.milian.dev@gmail.com>
2025-07-03 12:32:40 +0200

    we don't use avante, claude code

--- init.lua
+++ init.lua
@@ -1045,7 +1045,7 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.snacks',
-  require 'kickstart.plugins.avante',
+  -- require 'kickstart.plugins.avante',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
