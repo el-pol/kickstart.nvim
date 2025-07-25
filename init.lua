6e69b9befb34d959a851542c568b0517a7c5283a
Pol Milian <pol.milian.dev@gmail.com>
2025-07-25 17:59:40 +0200

    add winborder

--- init.lua
+++ init.lua
@@ -110,6 +110,9 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- Adds border (fix for LSP), but breaks Telescope (TODO)
+vim.opt.winborder = 'single'
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
