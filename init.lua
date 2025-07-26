bf7a132b45229fcbc8a27e750ba7b340a21963ea
Pol Milian <pol.milian.dev@gmail.com>
2025-07-26 18:26:23 +0200

    no need

--- init.lua
+++ init.lua
@@ -849,18 +849,6 @@ require('lazy').setup({
     },
   },
 
-  -- {
-  --   'zenbones-theme/zenbones.nvim',
-  -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-  -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-  -- In Vim, compat mode is turned on as Lush only works in Neovim.
-  --   dependencies = 'rktjmp/lush.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = true,
-  --   opts = {},
-  -- },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
